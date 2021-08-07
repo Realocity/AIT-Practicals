6119319219a6390e95a3e309c99a311a1296ce97
shubham sapkal <ssapkal101@gmail.com>
2021-08-07 14:08:31 +0530

    code compacted

--- Practical 1/Q1.js
+++ Practical 1/Q1.js
@@ -2,12 +2,6 @@
 
 // Program to show current date and time using user defined module
 
-const fs = require("fs");
-
 var today = new Date();
-
-const name = process.argv[2];
-
-fs.mkdirSync(name);
 
 console.log("The Current Date and Time is : " + today);
